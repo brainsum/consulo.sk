cc888a928cc9ae8951e0c2c291653f6e21945dd9
Krisztian Pinter <keeteean@gmail.com>
2019-05-20 12:13:23 +0200

    CON-000 | Moved slider configs to slider library.

---
+++ web/themes/custom/consulo_theme/js/src/sliders.js
@@ -0,0 +1,117 @@
+/**
+ * @file
+ * Slick slider configs.
+ */
+
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.frontpageSlider = {
+    attach: function (context) {
+      var slickElement = $('.slick_slider', context);
+
+      if (slickElement.length > 0) {
+        slickElement.not('.slick-initialized').slick({
+          adaptiveHeight: false,
+          accessibility: false,
+          arrows: true,
+          prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="icon icon--arrow-left"></i></button>',
+          nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="icon icon--arrow-right"></i></button>',
+          autoplay: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplaySpeed: 4000,
+          speed: 800,
+          dots: true,
+          fade: true,
+          pauseOnFocus: true,
+          swipe: true
+        });
+      }
+    }
+  };
+
+  Drupal.behaviors.aboutSlider = {
+    attach: function (context) {
+
+      if ($('.slick_slider_4', context).length > 0) {
+        $('.slick_slider_4', context).not('.slick-initialized').slick({
+          accessibility: false,
+          arrows: false,
+          autoplay: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          lazyLoad: 'ondemand',
+          adaptiveHeight: true,
+          autoplaySpeed: 3000,
+          speed: 800,
+          dots: false,
+          fade: true,
+          pauseOnFocus: true,
+          swipe: true
+        });
+      }
+
+      if ($('.slick_slider_5', context).length > 0) {
+        $('.slick_slider_5', context).not('.slick-initialized').slick({
+          accessibility: false,
+          arrows: false,
+          autoplay: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          lazyLoad: 'ondemand',
+          adaptiveHeight: true,
+          autoplaySpeed: 3000,
+          speed: 800,
+          dots: false,
+          fade: true,
+          pauseOnFocus: true,
+          swipe: true
+        });
+      }
+
+      if ($('.slick_slider_6', context).length > 0) {
+        $('.slick_slider_6', context).not('.slick-initialized').slick({
+          accessibility: false,
+          arrows: false,
+          autoplay: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          lazyLoad: 'ondemand',
+          adaptiveHeight: true,
+          autoplaySpeed: 3000,
+          speed: 800,
+          dots: false,
+          fade: true,
+          pauseOnFocus: true,
+          swipe: true
+        });
+      }
+    }
+  };
+
+  Drupal.behaviors.quotesslider = {
+    attach: function (context) {
+      var quouesSlider = $('.view-partner-quotes > .view-content', context);
+
+      if (quouesSlider.length > 0 ) {
+        quouesSlider.slick({
+          accessibility: false,
+          arrows: true,
+          prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="icon icon--arrow-left"></i></button>',
+          nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="icon icon--arrow-right"></i></button>',
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          lazyLoad: 'ondemand',
+          adaptiveHeight: true,
+          autoplaySpeed: 4000,
+          speed: 800,
+          dots: true,
+          pauseOnFocus: true,
+          swipe: true
+        });
+      }
+    }
+  };
+
+})(jQuery);
